01f6d256990b75e84bfb7e9131f9bee176f2db83
Jan jun <96610257+MJN-ID-zero@users.noreply.github.com>
2021-12-26 12:44:57 +0000

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,47 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+figlet login | lolcat
+read -p "masukan nama lu = "nama
+echo
+toilet -f big -F gay by $nama
+echo $green"selamat datang $nama"
+echo
+sleep 2
+echo $yellow"silahkan pilih"
+echo
+echo $cyan"1.) stabilkan jaringan"
+echo
+echo $blue"0.) keluar aja !!!"
+echo
+echo $white
+read -p "pilih mana :"bro
+
+if [ $bro = 1 ] || [ $bro = 1]
+then
+clear
+toilet -f big -F gay jan
+echo "=========================="
+echo "Author : juund | lolcat
+echo "=========================="
+echo $green"stabilkan jaringan by $nama"
+sleep 2
+figlet mulai | lolcat
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $yelloow
+figlet "bye"
+echo "white "To Be Continued"
+sleep 3
+exit
+fi
