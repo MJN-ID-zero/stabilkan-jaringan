0fc75927a2b7112dae1b4b6847d15a131f1c9a5e
Jan jun <96610257+MJN-ID-zero@users.noreply.github.com>
2021-12-27 01:33:00 +0000

    Update Gass.sh

--- Gass.sh
+++ Gass.sh
@@ -1,43 +1,35 @@
 #!/bin/bash
-
-clear
-blue='\033[34;1m'
-green='\033[32;1m'
-purple='\033[35;1m'
-cyan='\033[36;1m'
-red='\033[31;1m'
-white='\033[37;1m'
-yellow='\033[33;1m'
 sleep 1
 echo
 toilet -f big -F gay By jan jun
-echo $green"Selamat Datang"
+echo
+echo "Selamat Datang"
 echo
 sleep 2
-echo $yellow"Silahkan Pilih Tools :"
+echo "Silahkan Pilih Tools :"
 echo
-echo $cyan"1.)Stabilkan Jaringan"
+echo "1.)Stabilkan Jaringan" | lolcat
 echo
-echo $blue"0.)Keluar !!!"
+echo "0.)Keluar !!!"
 echo
-echo $white
+echo 
 read -p "pilih mana :" bro
 
 if [ $bro = 1 ] || [ $bro = 1 ]
 then
 clear
 toilet -f big -F jan
-echo $green"Stabilkan Jaringan By MJN"
+echo "Stabilkan Jaringan By MJN"
 sleep 2
-ping -s1000 1.1.1.1
+ping -s1000 1.1.1.1 | lolcat
 fi
 
 if [ $bro = 0 ] || [ $bro = 0 ]
 then
 clear
-echo $yellow
+echo 
 figlet"Bay"
-echo $white"To Be Continued"
+echo "To Be Continued" | lolcat
 sleep 3
 exit
 fi
