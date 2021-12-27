aef2e63ddc81d380475378b9ff3814f8d0e986dc
Jan jun <96610257+MJN-ID-zero@users.noreply.github.com>
2021-12-27 02:46:26 +0000

    Update Gass.sh

--- Gass.sh
+++ Gass.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+figlet Login | lolcat
+read -p "masukan nama lu = "nama
+if [ $nama = $nama ];
+then
+echo
+figlet welcome
+echo
+figlet $nama
+sleep 3
 clear
 sleep 1
 echo
-toilet -f big -F gay By jan jun
+toilet -f big -F gay By $nama
 echo
-echo "Selamat Datang"
-echo
+echo "==========================="
+echo "Author = MJN" | lolcat
+echo "==========================="
 sleep 2
-echo "Silahkan Pilih Tools :"
+echo
+echo "Silahkan Pilih :"
 echo
 echo "1.)Stabilkan Jaringan" | lolcat
 echo
@@ -19,7 +30,7 @@ read -p "pilih mana :" bro
 if [ $bro = 1 ] || [ $bro = 1 ]
 then
 clear
-toilet -f big -F gay jan
+toilet -f big -F gay $nama
 echo "Stabilkan Jaringan By MJN"
 sleep 2
 ping -s1000 1.1.1.1 | lolcat
@@ -29,7 +40,7 @@ if [ $bro = 0 ] || [ $bro = 0 ]
 then
 clear
 echo 
-figlet"Bay"
+figlet"Bay $nama"
 echo "To Be Continued" | lolcat
 sleep 3
 exit
